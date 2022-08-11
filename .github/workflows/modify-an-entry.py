2c6107b76e6ebdae148a579a4e3db42eb7f1895e
Mark Mutti <mmutti@aspiration.com>
2022-08-10 20:30:50 -0700

    Adds new test to deployment pipeline

--- .github/workflows/modify-an-entry.py
+++ .github/workflows/modify-an-entry.py
@@ -8,6 +8,6 @@ inci_db = tinydb.Query()
 inci = db.search(inci_db.type == 'Wildfire')
 inci = inci[0]
 
-inci['resources'] = 'ENG-10 ENG-19 ENG-310'
+inci['resources'] = 'MM-1 MM-10 MM-100'
 
 db.update(inci,inci_db.id == inci['id'])
